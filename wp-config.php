330dcf9145e50e0ac2f6ce5b7c4bdcc657977f7d
Nont Banditwong <nontster@gmail.com>
2014-04-24 13:37:13 +0700

    cleanup wp-config.php, move some variables to proper code block

--- wp-config.php
+++ wp-config.php
@@ -37,61 +37,61 @@ if ( file_exists( dirname( __FILE__ ) . '/wp-config-production.php' ) ) {
 
 	/** The Database Collate type. Don't change this if in doubt. */
 	define('DB_COLLATE', '');
+
+	/* defined after managed by git */
+	define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+	define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+	define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+	define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+
+	/**#@+
+ 	* Authentication Unique Keys and Salts.
+ 	*
+ 	* Change these to different unique phrases!
+ 	* You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ 	* You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ 	*
+ 	* @since 2.6.0
+ 	*/
+	define('AUTH_KEY',         'put your unique phrase here');
+	define('SECURE_AUTH_KEY',  'put your unique phrase here');
+	define('LOGGED_IN_KEY',    'put your unique phrase here');
+	define('NONCE_KEY',        'put your unique phrase here');
+	define('AUTH_SALT',        'put your unique phrase here');
+	define('SECURE_AUTH_SALT', 'put your unique phrase here');
+	define('LOGGED_IN_SALT',   'put your unique phrase here');
+	define('NONCE_SALT',       'put your unique phrase here');
+
+	/**#@-*/
+
+	/**
+ 	* WordPress Database Table prefix.
+ 	*
+ 	* You can have multiple installations in one database if you give each a unique
+ 	* prefix. Only numbers, letters, and underscores please!
+ 	*/
+	$table_prefix  = 'wp_';
+
+	/**
+ 	* WordPress Localized Language, defaults to English.
+ 	*
+ 	* Change this to localize WordPress. A corresponding MO file for the chosen
+ 	* language must be installed to wp-content/languages. For example, install
+ 	* de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ 	* language support.
+ 	*/
+	define('WPLANG', '');
+
+	/**
+ 	* For developers: WordPress debugging mode.
+ 	*
+ 	* Change this to true to enable the display of notices during development.
+ 	* It is strongly recommended that plugin and theme developers use WP_DEBUG
+ 	* in their development environments.
+ 	*/
+	define('WP_DEBUG', true);
+
 }
-
-/* defined after managed by git */
-define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
-define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
-define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
-define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
-
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = 'wp_';
-
-/**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
-define('WPLANG', '');
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
